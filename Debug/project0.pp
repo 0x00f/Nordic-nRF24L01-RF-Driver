e2e701368b1473b7eb04684be10296c6cda1a323
rohitdureja <rohit@seas.upenn.edu>
2014-07-29 12:05:16 -0400

    created repository

---
+++ Debug/project0.pp
@@ -0,0 +1,25 @@
+# FIXED
+
+project0.obj: ../project0.c
+project0.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdint.h
+project0.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdbool.h
+project0.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/yvals.h
+project0.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdarg.h
+project0.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/linkage.h
+project0.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/_lock.h
+project0.obj: C:/ti/TivaWare_C_Series-1.1/inc/hw_types.h
+project0.obj: C:/ti/TivaWare_C_Series-1.1/inc/hw_memmap.h
+project0.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/sysctl.h
+project0.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/gpio.h
+
+../project0.c: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdint.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdbool.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/yvals.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/_lock.h: 
+C:/ti/TivaWare_C_Series-1.1/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-1.1/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/gpio.h: 
